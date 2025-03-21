0970468b33e47fc4055525ec36e74bd38b0dec06
nick <nguyenhaininh333@gmail.com>
2025-03-21 17:26:04 +0700

    local template

---
+++ local/mycustommessage/version.php
@@ -0,0 +1,8 @@
+<?php
+defined('MOODLE_INTERNAL') || die();
+
+$plugin->component = 'local_custommessage';
+$plugin->version = 2025032100;
+$plugin->requires = 2022041900;
+$plugin->maturity = MATURITY_STABLE;
+$plugin->release = '1.0';
